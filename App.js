1ead3cf05ebe22a02ef4c7fd17052dcd8cb2f492
sergiojaa <119433399+sergiojaa@users.noreply.github.com>
2024-03-16 23:36:09 +0400

    Add files via upload

--- App.js
+++ App.js
@@ -1,10 +1,12 @@
-import logo from './logo.svg';
+
 import './App.css';
 import ProductInfo from './component/ProductInfo';
+import Header from './Header';
 
 function App() {
   return (
     <div className="App">
+      <Header/>
       <ProductInfo/>
     </div>
   );
