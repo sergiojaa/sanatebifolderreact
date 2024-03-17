497fe9e1bcef6cdcee45cd0c7af4aae5cdd07158
sergiojaa <119433399+sergiojaa@users.noreply.github.com>
2024-03-17 21:52:45 +0400

    Add files via upload

--- App.js
+++ App.js
@@ -2,11 +2,13 @@
 import './App.css';
 import ProductInfo from './component/ProductInfo';
 import Header from './Header';
+import Satauri from './Satauri';
 
 function App() {
   return (
     <div className="App">
       <Header/>
+      <Satauri/>
       <ProductInfo/>
     </div>
   );
