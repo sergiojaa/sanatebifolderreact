50ff72cea19fc7a39b7ff5387ad461beb3d10904
sergiojaa <119433399+sergiojaa@users.noreply.github.com>
2024-03-17 21:52:21 +0400

    Add files via upload

---
+++ Satauri.jsx
@@ -0,0 +1,9 @@
+const Satauri = () =>{
+    return <div>
+        <div class="kategori">
+            <p class="kategoria">კატეგორია:</p>
+            <a  class="sanatebi" href="sanatebi.html">სანათები</a>
+        </div>
+    </div>
+}
+export default Satauri
